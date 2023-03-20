681182a8de87e4f13d5fd49b83337692973be66f
Mlkz <55971701+M8825@users.noreply.github.com>
2023-03-20 00:46:26 -0400

    Add grab address page styling

--- frontend/src/components/CreateListing/GrabAddress.jsx
+++ frontend/src/components/CreateListing/GrabAddress.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import ListingForm from "../ListingForm/ListingForm";
+import SelectedState from "./SelectedState";
 
 import "./GrabAddress.scss";
-import SelectedState from "./SelectedState";
 
 const GrabAddress = () => {
 	const [nextPage, setNextPage] = useState(null);
@@ -48,52 +48,79 @@ const GrabAddress = () => {
 	return nextPage ? (
 		<ListingForm />
 	) : (
-		<form className="map-confirmation">
-			<div className="trapezoid-wrapper">
-				<div className="rectangle">
-					<p>Post a For Sale by Owner Listing</p>
+		<div className="address-input-wrapper">
+			<form className="address-input">
+				<div className="trapezoid-wrapper">
+					<div className="rectangle">
+						<p>Post a For Sale by Owner Listing</p>
+					</div>
+					<div className="trapezoid"></div>
 				</div>
-				<div className="trapezoid"></div>
+				<div className="grab-address">
+					<input
+						type="text"
+						onChange={(e) =>
+							handleAddressChange(e, "streetAddress")
+						}
+						placeholder="Street address"
+						value={address.streetAddress}
+					/>
+
+					<input
+						type="text"
+						onChange={(e) => handleAddressChange(e, "unit")}
+						placeholder="Unit# (optional)"
+						value={address.streetAddress}
+					/>
+
+					<input
+						type="text"
+						placeholder="City"
+						value={address.city}
+						onChange={(e) => handleAddressChange(e, "city")}
+					/>
+
+					<SelectedState handleAddressChange={handleAddressChange} />
+
+					<input
+						type="text"
+						placeholder="Zip code"
+						value={address.zipcode}
+						onChange={(e) => handleAddressChange(e, "zipcode")}
+					/>
+					<button
+						className="submit-btn"
+						type="submit"
+						onClick={handleSubmit}
+					>
+						Continue
+					</button>
+				</div>
+			</form>
+			<h2>Why post on Villow?</h2>
+			<div className="address-input-bottom">
+				<div className="description-items">
+					<div className="home-image"></div>
+					<h5>
+						Post a listing for free, including video and unlimited
+						photos.
+					</h5>
+				</div>
+				<div className="description-items">
+					<div className="rupor-image"></div>
+					<h5>
+						Your home will be listed on Villow and Trulia, reaching
+						the largest audience of home shoppers on the Web.
+					</h5>
+				</div>
+				<div className="description-items">
+					<div className="check-mark"></div>
+					<h5>
+						Home shoppers receive instant emails about new listings.
+					</h5>
+				</div>
 			</div>
-			<div className="grab-address">
-				<input
-					type="text"
-					onChange={(e) => handleAddressChange(e, "streetAddress")}
-					placeholder="Street address"
-					value={address.streetAddress}
-				/>
-
-				<input
-					type="text"
-					onChange={(e) => handleAddressChange(e, "unit")}
-					placeholder="Unit# (optional)"
-					value={address.streetAddress}
-				/>
-
-				<input
-					type="text"
-					placeholder="City"
-					value={address.city}
-					onChange={(e) => handleAddressChange(e, "city")}
-				/>
-
-				<SelectedState handleAddressChange={handleAddressChange} />
-
-				<input
-					type="text"
-					placeholder="Zip code"
-					value={address.zipcode}
-					onChange={(e) => handleAddressChange(e, "zipcode")}
-				/>
-				<button
-					className="submit-btn"
-					type="submit"
-					onClick={handleSubmit}
-				>
-					Continue
-				</button>
-			</div>
-		</form>
+		</div>
 	);
 };
 
