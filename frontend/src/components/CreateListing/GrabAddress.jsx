16032952b27c5abc11069c382ce6203b6736e05b
Mlkz <55971701+M8825@users.noreply.github.com>
2023-03-20 01:45:34 -0400

    Separate address form from address component

--- frontend/src/components/CreateListing/GrabAddress.jsx
+++ frontend/src/components/CreateListing/GrabAddress.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import ListingForm from "../ListingForm/ListingForm";
-import SelectedState from "./SelectedState";
+import GrabAddressForm from "./GrabAddressForm";
 
 import "./GrabAddress.scss";
 
 const GrabAddress = () => {
-	const [nextPage, setNextPage] = useState(null);
+	const [nextPage, setNextPage] = useState(false);
 
 	const [address, setAddress] = useState({
 		streetAddress: "",
@@ -103,98 +103,12 @@ const GrabAddress = () => {
 	return nextPage ? (
 		<ListingForm address={address} />
 	) : (
-		<div className="address-input-wrapper">
-			<form className="address-input">
-				<div className="trapezoid-wrapper">
-					<div className="rectangle">
-						<p>Post a For Sale by Owner Listing</p>
-					</div>
-					<div className="trapezoid"></div>
-				</div>
-				<div className="grab-address">
-					<div className="input-wrapper">
-						<input
-							type="text"
-							onChange={(e) =>
-								handleAddressChange(e, "streetAddress")
-							}
-							placeholder="Street address"
-							value={address.streetAddress}
-						/>
-
-						<p>{errors.isSet && errors.streetAddress}</p>
-					</div>
-
-					<div className="input-wrapper">
-						<input
-							type="text"
-							onChange={(e) => handleAddressChange(e, "unit")}
-							placeholder="Unit# (optional)"
-							value={address.unit}
-						/>
-						<p>{errors.isSet && " "}</p>
-					</div>
-
-					<div className="input-wrapper">
-						<input
-							type="text"
-							placeholder="City"
-							value={address.city}
-							onChange={(e) => handleAddressChange(e, "city")}
-						/>
-						<p>{errors.isSet && errors.city}</p>
-					</div>
-
-					<div className="input-wrapper">
-						<SelectedState
-							handleAddressChange={handleAddressChange}
-						/>
-						<p>{errors.isSet && errors.state}</p>
-					</div>
-
-					<div className="input-wrapper">
-						<input
-							type="text"
-							placeholder="Zip code"
-							value={address.zipcode}
-							onChange={(e) => handleAddressChange(e, "zipcode")}
-						/>
-						<p>{errors.isSet && errors.zipcode}</p>
-					</div>
-
-					<button
-						className="submit-btn"
-						type="submit"
-						onClick={handleSubmit}
-					>
-						Continue
-					</button>
-				</div>
-			</form>
-			<h2>Why post on Villow?</h2>
-			<div className="address-input-bottom">
-				<div className="description-items">
-					<div className="home-image"></div>
-					<h5>
-						Post a listing for free, including video and unlimited
-						photos.
-					</h5>
-				</div>
-				<div className="description-items">
-					<div className="rupor-image"></div>
-					<h5>
-						Your home will be listed on Villow and Trulia, reaching
-						the largest audience of home shoppers on the Web.
-					</h5>
-				</div>
-				<div className="description-items">
-					<div className="check-mark"></div>
-					<h5>
-						Home shoppers receive instant emails about new listings.
-					</h5>
-				</div>
-			</div>
-		</div>
+		<GrabAddressForm
+			address={address}
+			handleAddressChange={handleAddressChange}
+			errors={errors}
+			handleSubmit={handleSubmit}
+		/>
 	);
 };
 
