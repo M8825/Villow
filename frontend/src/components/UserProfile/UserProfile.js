bd93bead0029d5cf96d2662d2fdcb1191eecf62e
Mlkz <55971701+M8825@users.noreply.github.com>
2023-03-22 14:28:47 -0400

    Add tabs to user profile

--- frontend/src/components/UserProfile/UserProfile.js
+++ frontend/src/components/UserProfile/UserProfile.js
@@ -1,7 +1,47 @@
+import Navigation from "../Header/Navigation";
+import tabListTheme from "../Modal/ModalTabsTheme";
+import {
+	ChakraProvider,
+	Tabs,
+	TabList,
+	TabPanels,
+	Tab,
+	TabPanel,
+} from "@chakra-ui/react";
+import Footer from "../Footer";
+
+import "./UserProfile.scss";
+
 const UserProfile = () => {
-    return (
-        <h1>FOO from user profile</h1>
-    )
+	return (
+		<>
+			<Navigation isIndex={true} />
+			<div className="profile-wrapper">
+				<div className="content">
+					<ChakraProvider theme={tabListTheme}>
+						<Tabs>
+							<TabList
+								borderBottom={"1px solid "}
+								borderColor={"rgb(209 209 213)"}
+							>
+								<Tab className="form-tab">Your Home</Tab>
+								<Tab>Favorite Homes</Tab>
+							</TabList>
+							<TabPanels>
+								<TabPanel>
+									<h1>Foo</h1>
+								</TabPanel>
+								<TabPanel>
+									<h1>Bar</h1>
+								</TabPanel>
+							</TabPanels>
+						</Tabs>
+					</ChakraProvider>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
 };
 
 export default UserProfile;
