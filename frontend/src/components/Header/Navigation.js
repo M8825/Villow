fafea8f4cff5e296840305a3a7e3913fe9823619
Mlkz <55971701+M8825@users.noreply.github.com>
2023-04-16 18:35:40 -0400

    Ref: fix <Link> to home page related error in navigation

--- frontend/src/components/Header/Navigation.js
+++ frontend/src/components/Header/Navigation.js
@@ -1,121 +1,83 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Link } from "react-router-dom";
+
 import ModalContainer from "../Modal/ModalContainer";
 import AuthorizedUser from "./AuthorizedUser";
 import ModalTabs from "../Modal/ModalTabs";
 import ModalWelcomeHeader from "./Welcome";
-import { Link } from "react-router-dom";
-import Layout from "../Cards";
-import Footer from "../Footer";
-
-import ListingsCarousel from "../ListingsCarousel/ListingsCarousel";
-import villow from "../assets/villow.png";
-
-import "./Navigation.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchCurrentUser, getActiveUser } from "../../store/usersReducer";
 import SearchBar from "../SearchBar/SearchBar";
 
+import { fetchCurrentUser, getActiveUser } from "../../store/usersReducer";
+
+import "./Navigation.scss";
+import villow from "../assets/villow.png";
+
 const Navigation = ({ isIndex }) => {
-	const dispatch = useDispatch();
-	const activeUser = useSelector(getActiveUser());
+  const dispatch = useDispatch();
+  const activeUser = useSelector(getActiveUser());
 
-	useEffect(() => {
-		if (!activeUser){
-			dispatch(fetchCurrentUser());
-		}
-	}, [activeUser]);
+  useEffect(() => {
+    if (!activeUser) {
+      dispatch(fetchCurrentUser());
+    }
+  }, [activeUser]);
 
-	const modalAreaStyling = {
-		display: "flex",
-		flexDirection: "column",
-		padding: "27px 16px 14px 16px",
-		borderRadius: "10px",
-		width: "456px",
-		backgroundColor: "rgb(255 255 255)",
-	};
+  const modalAreaStyling = {
+    display: "flex",
+    flexDirection: "column",
+    padding: "27px 16px 14px 16px",
+    borderRadius: "10px",
+    width: "456px",
+    backgroundColor: "rgb(255 255 255)",
+  };
 
-	return (
-		<>
-			<div className="container">
-				<nav id="navigation">
-					<div className="grid-item left">
-						<a href="https://github.com/M8825">Github</a>
-						<a href="https://www.linkedin.com/in/malkhaz-mamulashvili-703a97208/">
-							LinkedIn
-						</a>
-						<a href="https://wellfound.com/u/malkhaz-mamulashvili">
-							Wellfound
-						</a>
-						<a href="#">About</a>
-					</div>
-					<Link
-						to="/"
-						render={() => {
-							return (
-								<>
-									<Navigation />
-									<ListingsCarousel
-										header={"Homes For You in New York, NY"}
-										paragraph={"Based on your view history"}
-									/>
-									<Layout />
-									<ListingsCarousel
-										header={
-											"Trending Homes in New York, NY"
-										}
-										paragraph={
-											"Popular listings in the area"
-										}
-									/>
-									<ListingsCarousel
-										header={
-											"Selling Soon Homes in New York, NY"
-										}
-										paragraph={
-											"Likely to sell faster than 80% of homes nearby"
-										}
-									/>
-									<Footer />
-								</>
-							);
-						}}
-					>
-						<div
-							className="grid-item middle"
-							style={{ width: "200px", height: "10px" }}
-						>
-							<img
-								src={villow}
-								alt="villow"
-								style={{ marginTop: "10px" }}
-							/>
-						</div>
-					</Link>
-					<div className="grid-item right">
-						<p>Manage Rentals</p>
-						<p>Advertise</p>
-						<p>Help</p>
-						{activeUser ? (
-							<AuthorizedUser />
-						) : (
-							<ModalContainer
-								modalAreaStyling={modalAreaStyling}
-								ModalWelcomeHeader={ModalWelcomeHeader}
-								ModalTabs={ModalTabs}
-							/>
-						)}
-					</div>
-				</nav>
-				{!isIndex && (
-					<div className="search_container">
-						<h1>Find it. Tour it. Own it.</h1>
-						<SearchBar />
-					</div>
-				)}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="container">
+        <nav id="navigation">
+          <div className="grid-item left">
+            <a href="https://github.com/M8825">Github</a>
+            <a href="https://www.linkedin.com/in/malkhaz-mamulashvili-703a97208/">
+              LinkedIn
+            </a>
+            <a href="https://wellfound.com/u/malkhaz-mamulashvili">Wellfound</a>
+            <a href="#">About</a>
+          </div>
+
+          <Link to="/">
+            <div
+              className="grid-item middle"
+              style={{ width: "200px", height: "10px" }}
+            >
+              <img src={villow} alt="villow" style={{ marginTop: "10px" }} />
+            </div>
+          </Link>
+          <div className="grid-item right">
+            <p>Manage Rentals</p>
+            <p>Advertise</p>
+            <p>Help</p>
+            {activeUser ? (
+              <AuthorizedUser />
+            ) : (
+              <ModalContainer
+                modalAreaStyling={modalAreaStyling}
+                ModalWelcomeHeader={ModalWelcomeHeader}
+                ModalTabs={ModalTabs}
+              />
+            )}
+          </div>
+        </nav>
+        {!isIndex && (
+          <div className="search_container">
+            <h1>Find it. Tour it. Own it.</h1>
+            <SearchBar />
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Navigation;
