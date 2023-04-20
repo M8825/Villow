0e90288583c68855fc592ec1952b6d3405d8df67
Mlkz <55971701+M8825@users.noreply.github.com>
2023-04-20 13:52:51 -0400

    add: Fix click on deselect button

--- frontend/src/components/SearchBar/FilterButtons/HomeType/HomeType.js
+++ frontend/src/components/SearchBar/FilterButtons/HomeType/HomeType.js
@@ -1,21 +1,35 @@
-import DropDown from "../DropDown";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
+
+import DropDown from "../DropDown";
 
 import "./HomeType.scss";
 
 const HomeType = () => {
+  const [deselctAll, setDeselctAll] = useState("Deselect All");
+
+  function handleDeselectClick(e) {
+    e.preventDefault();
+
+    if (deselctAll === "Deselect All") {
+      setDeselctAll("Select All");
+    } else {
+      setDeselctAll("Deselect All");
+    }
+  }
+
   return (
     <DropDown buttonValue={"Home Type"} selectedButton={true}>
       <div className="home-type-container">
         <div className="deselect-all-title-container">
           <h6>Home Type</h6>
 
-          <div className="deselect-btn-container">
+          <div className="deselect-btn-container" onClick={handleDeselectClick}>
             <button className="deselect-btn">
-              <FontAwesomeIcon icon={faCheck} className="icon"/>
+              <FontAwesomeIcon icon={faCheck} className="icon" />
             </button>
-            <h6 className="deselect-text">Deselect All</h6>
+            <h6 className="deselect-text">{deselctAll}</h6>
           </div>
         </div>
       </div>
