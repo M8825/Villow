ba44278ef4864811392ffcf5f656c7278246f3f8
Mlkz <55971701+M8825@users.noreply.github.com>
2023-04-21 11:20:29 -0400

    refactor: BedsAndBaths button value

--- frontend/src/components/SearchBar/FilterButtons/BedsAndBaths/BedsAndBaths.js
+++ frontend/src/components/SearchBar/FilterButtons/BedsAndBaths/BedsAndBaths.js
@@ -1,16 +1,17 @@
 import { useSelector } from "react-redux";
-import { useEffect, useState  } from "react";
+import { useEffect, useState } from "react";
 
 import DropDown from "../DropDown";
 import Squares from "./Squares";
 
-import "./BedsAndBaths.scss";
 import {
   getNumberOfBathrooms,
   getNumberOfBedrooms,
   setBathroom,
   setBedroom,
 } from "../../../../store/searchFilters";
+
+import "./BedsAndBaths.scss";
 
 const BedsAndBaths = () => {
   const numberOfBedrooms = useSelector(getNumberOfBedrooms());
@@ -19,50 +20,50 @@ const BedsAndBaths = () => {
   const [selectedButton, setSelectedButton] = useState(false);
 
   useEffect(() => {
-      if (numberOfBedrooms !== "0" || numberOfBathrooms !== "0") {
-        setSelectedButton(true);
-      }
+    if (numberOfBedrooms !== "0" || numberOfBathrooms !== "0") {
+      setSelectedButton(true);
+    }
 
     return () => {
       setSelectedButton(false);
-    }
+    };
   }, [numberOfBedrooms, numberOfBathrooms]);
 
   return (
-      <DropDown
-        selectedButton={selectedButton}
-        buttonValue={
-          numberOfBedrooms === "0" && numberOfBathrooms === "0"
-            ? "Beds & Baths"
-            : numberOfBedrooms + " +bd" + " " + numberOfBathrooms + " +ba"
-        }
-      >
-        <h6 className="title">Number of Bedrooms</h6>
+    <DropDown
+      selectedButton={selectedButton}
+      buttonValue={
+        numberOfBedrooms === "0" && numberOfBathrooms === "0"
+          ? "Beds & Baths"
+          : `${numberOfBedrooms}+ bd, ${numberOfBathrooms}+ ba`
+      }
+    >
+      <h6 className="title">Number of Bedrooms</h6>
 
-        <div className="beds-and-baths">
-          <label>
-            <legend>Bedrooms</legend>
-            <Squares
-              squareType={"bedroom"}
-              squareNumber={numberOfBedrooms}
-              setNumber={setBedroom}
-            />
-          </label>
-        </div>
+      <div className="beds-and-baths">
+        <label>
+          <legend>Bedrooms</legend>
+          <Squares
+            squareType={"bedroom"}
+            squareNumber={numberOfBedrooms}
+            setNumber={setBedroom}
+          />
+        </label>
+      </div>
 
-        <h6 className="title">Number of Bathrooms</h6>
+      <h6 className="title">Number of Bathrooms</h6>
 
-        <div className="beds-and-baths">
-          <label>
-            <legend>Bathrooms</legend>
-            <Squares
-              squareType={"bathroom"}
-              squareNumber={numberOfBathrooms}
-              setNumber={setBathroom}
-            />
-          </label>
-        </div>
-      </DropDown>
+      <div className="beds-and-baths">
+        <label>
+          <legend>Bathrooms</legend>
+          <Squares
+            squareType={"bathroom"}
+            squareNumber={numberOfBathrooms}
+            setNumber={setBathroom}
+          />
+        </label>
+      </div>
+    </DropDown>
   );
 };
 
