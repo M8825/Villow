3b890037fe02a08605d6c6995252359fb83734c1
Mlkz <55971701+M8825@users.noreply.github.com>
2023-04-22 23:25:04 -0400

    Restore the SuggestionItem component

---
+++ frontend/src/components/SearchBar/SuggestionItem.js
@@ -0,0 +1,54 @@
+import { useDispatch } from "react-redux";
+import { useHistory, useLocation } from "react-router-dom";
+
+import { setSearchWord } from "../../store/searchFilters";
+import { findMatchingIndices } from "./searchUtils";
+
+import "./SuggestionItem.scss";
+
+const SuggestionItem = ({ term, suggestion, value }) => {
+	const location = useLocation();
+	const dispatch = useDispatch();
+	const history = useHistory();
+
+  // returns start and end indecies of the matching substring
+  // of the suggestion and the value. It's beeing used to
+  // highlight the matching substring in the suggestion
+  const [start, end] = findMatchingIndices(suggestion, value);
+
+
+	const splash = location.pathname === "/";
+
+	const handleSearchOnClickItem = (e) => {
+		e.preventDefault();
+
+		dispatch(setSearchWord(suggestion, term));
+
+		if (splash) {
+			history.push("/listings");
+		}
+	};
+
+  return (
+    <li
+      className={splash ? "suggestion-item-splash" : "suggestion-item-index"}
+      onClick={handleSearchOnClickItem}
+    >
+      {end ? (
+        <>
+          <p>
+            {suggestion.slice(0, start)}
+            <span className="highlighted">
+              {suggestion.slice(start, end + 1)}
+            </span>
+            <span>{suggestion.slice(end + 1)}</span>
+          </p>
+        </>
+      ) : (
+        <p>{suggestion}</p>
+      )}
+    </li>
+  );
+};
+
+export default SuggestionItem;
