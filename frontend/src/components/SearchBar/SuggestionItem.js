5ad26c9fa1decbf22b25dafd7129c64dceb35bd6
Mlkz <55971701+M8825@users.noreply.github.com>
2023-04-24 18:31:55 -0400

    style: Finish splash search dropdown

--- frontend/src/components/SearchBar/SuggestionItem.js
+++ frontend/src/components/SearchBar/SuggestionItem.js
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
 import { useHistory, useLocation } from "react-router-dom";
 
-import { setSearchHistory, setSearchWord } from "../../store/searchFilters";
+import { setSearchWord } from "../../store/searchFilters";
 import { findMatchingIndices } from "./searchUtils";
 
 import "./SuggestionItem.scss";
@@ -11,10 +11,11 @@ const SuggestionItem = ({ term, suggestion, value }) => {
 	const dispatch = useDispatch();
 	const history = useHistory();
 
-	// returns start and end indecies of the matching substring
-	// of the suggestion and the value. It's beeing used to
-	// highlight the matching substring in the suggestion
-	const [start, end] = findMatchingIndices(suggestion, value);
+  // returns start and end indecies of the matching substring
+  // of the suggestion and the value. It's beeing used to
+  // highlight the matching substring in the suggestion
+  const [start, end] = findMatchingIndices(suggestion, value);
+
 
 	const splash = location.pathname === "/";
 
@@ -22,30 +23,32 @@ const SuggestionItem = ({ term, suggestion, value }) => {
 		e.preventDefault();
 
 		dispatch(setSearchWord(suggestion, term));
-		dispatch(setSearchHistory(suggestion, term));
 
 		if (splash) {
 			history.push("/listings");
 		}
 	};
 
-	return (
-		<div className="suggestions-item-container" onClick={handleSearchOnClickItem}>
-			{end ? (
-				<>
-					<p className={"suggestion-item-splash"}>
-						{suggestion.slice(0, start)}
-						<span className="highlighted">
-							{suggestion.slice(start, end + 1)}
-						</span>
-						<span>{suggestion.slice(end + 1)}</span>
-					</p>
-				</>
-			) : (
-				<p className="suggestion-item-splash">{suggestion}</p>
-			)}
-		</div>
-	);
+  return (
+    <li
+      className={splash ? "suggestion-item-splash" : "suggestion-item-index"}
+      onClick={handleSearchOnClickItem}
+    >
+      {end ? (
+        <>
+          <p>
+            {suggestion.slice(0, start)}
+            <span className="highlighted">
+              {suggestion.slice(start, end + 1)}
+            </span>
+            <span>{suggestion.slice(end + 1)}</span>
+          </p>
+        </>
+      ) : (
+        <p>{suggestion}</p>
+      )}
+    </li>
+  );
 };
 
 export default SuggestionItem;
