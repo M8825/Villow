5d9164202a838ad536e9ac2f69e1426008b5f4f8
Mlkz <maho.mamulashvili@gmail.com>
2023-11-20 13:50:33 -0500

    Debugging: Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -20,6 +20,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*",
       headers: :any,
       methods: %i[get post put patch delete options head],
-      credentials: true
+      credentials: false
   end
 end
