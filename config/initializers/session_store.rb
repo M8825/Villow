ee51ccf5b25cea3a4bd9d4bdf58c0ff37ffe462e
Mlkz <maho.mamulashvili@gmail.com>
2023-11-21 00:10:06 -0500

    Deb: specify domain is session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
-Rails.application.config.session_store :cookie_store, key: 'villow', domain: :all, tld_length: 2, secure: Rails.env.production?
+Rails.application.config.session_store :cookie_store, key: '_villow_session', domain: 'villow-fe.onrender.com', secure: Rails.env.production?
 
