0b70010e8bfed96c4bb2af5c25f3105e2e2edb98
Mlkz <maho.mamulashvili@gmail.com>
2023-11-20 22:27:28 -0500

    Debugging: change cors credentions value to true

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -21,6 +21,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: %i[get post put patch delete options head],
       expose: ['X-CSRF-Token'],
-      credentials: false
+      credentials: true
   end
 end
