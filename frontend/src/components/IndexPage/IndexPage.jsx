33df5f2b77fea5e0a4776410ff7d48da9a81dd98
Mlkz <maho.mamulashvili@gmail.com>
2023-11-23 14:25:43 -0500

    Fix: IndexPage height overflow

--- frontend/src/components/IndexPage/IndexPage.jsx
+++ frontend/src/components/IndexPage/IndexPage.jsx
@@ -1,8 +1,9 @@
-import Listings from "../ListingsIndex/Listings";
-import SearchBar from "../SearchBar/SearchBar";
-
 import useLockBodyScroll from "./useLockScroll";
+
+import SearchBar from "../SearchBar/SearchBar";
 import Map from "../Map/map";
+
+import Listings from "../ListingsIndex/Listings";
 
 import "./IndexPage.scss";
 
@@ -13,9 +14,7 @@ const IndexPage = () => {
     <div className="index-page-container">
       <SearchBar />
       <div className="index-page-content-container">
-        <div className="map-wrapper">
           <Map />
-        </div>
         <Listings />
       </div>
     </div>
